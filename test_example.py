a825c71602b24b61372d19818e021f1710020023
Wakeru <cubix1061@gmail.com>
2025-10-11 20:52:24 -0400

    Testing the unit test

--- test_example.py
+++ test_example.py
@@ -1,6 +1,8 @@
 import unittest
 from example import addition
 
+# Please Start your file with "test_..." so the workflow can identify as it a test
+
 class TestExample(unittest.TestCase):
     def test_add(self):
         self.assertEqual(addition(2, 3), 5)  # Passes if add(2,3) == 5
