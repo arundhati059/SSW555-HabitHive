51aa51becdef57dd1f411866478943c227ebf295
Wakeru <cubix1061@gmail.com>
2025-10-11 20:28:11 -0400

    Test Case Example using python

---
+++ example.py
@@ -0,0 +1,7 @@
+def addition(a,b):
+    print("Hello world")
+    return a + b
+
+
+
+addition(2,3)
---
+++ test_example.py
@@ -0,0 +1,10 @@
+import unittest
+from example import addition
+
+class TestExample(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(addition(2, 3), 5)  # Passes if add(2,3) == 5
+        self.assertEqual(addition(-1, 1), 0) # Passes if add(-1,1) == 0
+
+if __name__ == '__main__':
+    unittest.main()
