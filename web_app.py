8fc8dae29d5d785f416d4bd44ab3078d21a1ff6f
Yasaswini <125636365+imposter7@users.noreply.github.com>
2025-11-05 19:19:08 -0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from HabitHive import AuthManager
 import os
 import firebase_admin
-from firebase_admin import auth, credentials
+from firebase_admin import auth, credentials, firestore
 import json
 
 app = Flask(__name__)
